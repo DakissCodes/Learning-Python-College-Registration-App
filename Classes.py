998e4872219176c56ab5fd4f14161fdda2d943f5
Justine Daquis <93424880+DakissCodes@users.noreply.github.com>
2022-06-09 14:50:22 +0800

    Final changes
    added some docs

--- Classes.py
+++ Classes.py
@@ -1,101 +1,164 @@
 
 '''
-PRODUCT INVENTORY PROJECT
+PRODUCT INVENTORY
 
 '''
 import time 
 import os
 
+def user_input(): # User prompt in terminal
+
+    prompt = input('\n[0] Exit application\n[1] Add New Product\n[2] Delete Existing Product\
+        \n[3] Delete All Products\n[4] Add Product Quantity\n[5] Remove Product Quantity\n[6] Edit Product\n[7] View Product\
+        \n[8] View All Products\n\nENTER: ')
+
+    return int(prompt)
 
 class Inventory:
-    def __init__(self):
+
+    def __init__(self): # List where products are stored
         self.products = []
 
-    def add(self,prod):
+    def add(self,prod): # Appends products to the list
         self.products.append(prod)
 
-    def delete_product(self,id_number):
+    def get_len(self):
+        if len(self.products) == 0:
+            print('Inventory is empty!')
+        return len(self.products)
+
+    def delete_product(self,id_number): # Deletes a product
+
         for _ in self.products:
+
             if _.id_number == id_number:
+
                 self.products.remove(_)
                 print('\nProduct successfully removed!')
                 return True
+
             else:
+
                 print('Product not found!')
                 return False
 
 
-    def add_quantity(self,quantity,id_number): # Add quantities to existing products
+    def add_quantity(self,quantity,id_number,product): # Add quantities to existing products
         for _ in self.products:
+
             if _.id_number == id_number:
+
                 _.quantity += quantity
+                print(product)
                 print('\nProduct quantity increased!')
 
-    def minus_quantity(self,quantity,id_number): # Substratcs quantities to existing products
+    def minus_quantity(self,quantity,id_number,product): # Substratcs quantities to existing products
 
         for _ in self.products:
+
             if _.id_number == id_number:
+
                 if _.quantity >= quantity:
                     _.quantity -= quantity
+                    print(product)
                     print('\nProduct successfully decreased!')    
-                    return True           
+                    return True  
+
                 else:
+
                     print('\nInsufficient amount!')
                     return False
-    def product_rename(self,id_number,index): # Edits product attributes
-        for _ in self.products:
-            if _.id_number == id_number:
-                
-                if index == 1: # Edit name
-                    new = '5'
-                    while new.isdigit():
-                        product_display(id_number)
-                        new = input('\nEnter New Product Name: ')
-                        if new.isdigit():
-                            print('\nMust be letters!')
 
-                    _.name = new
-                    os.system('cls')
-                    print('\nSuccessfully changed!')
-                elif index == 2: # Edit product ID
-                    new = ''
-                    while not new.isdigit(): 
-                        product_display(id_number)
+    def product_rename(self,id_number,product):   # Edits product attributes
+
+        index = int(input('\n[0] Exit\n[1] Edit name\n[2] Edit Product ID\n[3] Edit Price\nENTER: '))
+
+        if index == 1: # Edit name
+            time.sleep(.5)
+            os.system('cls')
+            print(product)
+            new = '5'
+
+            while new.isdigit():
+                new = input('\nEnter New Product Name: ')
+
+                if new.isdigit():
                     
-                        new = input('\nEnter new ID: ')
-                        if not new.isdigit():
-                            print('\nMust be numbers!')
+                    print('\nMust be letters!')
+                    time.sleep(.5)
+                    os.system('cls')
+                    print(product)
 
+            product.name = new
+            print('\nSuccessfully changed!')
+            
+        elif index == 2: # Edit product ID
+            time.sleep(.5)
+            os.system('cls')
+            print(product)                    
+            new = ''
+            while not new.isdigit(): 
 
-                    _.id_number = int(new)
-                    os.system('cls')            
-                    print('\nSuccessfully changed!')
-                elif index == 3: # Edit price
-                    new = ''
-                    while not new.isdigit():
-                        product_display(id_number)
-                        
-                        new = input('\nEnter New Product Price: ')
-                        if not new.isdigit():
-                            print('\nMust be numbers!')
+                new = input('\nEnter new ID: ')
 
-                    _.price = int(new)
-                    print('\nSuccessfully changed!')
-                elif index == 0:
-                    break
+                if not new.isdigit():
 
+                    print('\nMust be numbers!')
+                    time.sleep(.5)
+                    os.system('cls')
+                    print(product)
 
+            product.id_number = int(new)
+    
+            print('\nSuccessfully changed!')
 
-def product_display(id_num):
-    pass
-    #for _ in new_inventory.products:
-    #    if _.id_number == id_num:
-    #        print(_)
+        elif index == 3: # Edit price
+            time.sleep(.5)
+            os.system('cls')
+            print(product)
+            new = ''
+
+            while not new.isdigit():
+                
+                new = input('\nEnter New Product Price: ')
+
+                if not new.isdigit():
+
+                    print('\nMust be numbers!')
+                    time.sleep(.5)
+                    os.system('cls')
+                    print(product)
+
+            product.price = int(new)
+
+            print('\nSuccessfully changed!')
+        else:
+            return 0
+ 
+    def check_duplicate(self,id_num,name_given): # Checks whether name/id_number is already chosen
+        for _ in self.products:
+
+            if _.name == name_given:
+                return True
+            elif _.id_number == id_num:
+                return True
+            else:
+                continue
+            
+        return False
+        
+
+    def product_display(self,id_num): # Prints a product
+        for _ in self.products:
+            if _.id_number == id_num:
+                print(_)
+        return _
 
             
 
 
 class Product(Inventory):
+
     def __init__(self,name,id_number,price,quantity): # Adds a new type of product to the inventory
 
         self.name = name
@@ -103,12 +166,11 @@ class Product(Inventory):
         self.price = price
         self.quantity = quantity
     
-    def __str__(self):
+    def __str__(self): 
+
         return f'\nProduct Name: {self.name}\nProduct ID: {str(self.id_number)}\
             \nProduct Price: PHP {int(self.price)}\nProduct Quantity: {str(self.quantity)}'
-def user_input():
-    prompt = input('\n[0] Exit application\n[1] Add New Product\n[2] Delete Existing Product\
-        \n[3] Add Product Quantity\n[4] Remove Product Quantity\n[5] Edit Product\n[6] View Product\
-        \n[7] View All Products\n\nENTER:')
-    return int(prompt)
-#edit = input('[1] Edit Product Name:\n[2] Edit Product ID:\n[3] Edit Product Price:')
+
+
+new_inventory = Inventory() # Inventory
+
--- Logic.py
+++ Logic.py
@@ -1,202 +1,257 @@
 
-from pickle import FALSE
-import sys
 import Classes
 import time
 import os
-new_inventory = Classes.Inventory()
 
-user = ''
-new_inventory.add(Classes.Product('Test',1,150,100))
+
+user = '' 
+
 while user != 0:
-    print('\nLoading...')
-    time.sleep(.25)
-    os.system('cls')
-    user = int(Classes.user_input())
 
-    if user == 0:
+    time.sleep(.25) # Clears the terminal
+    os.system('cls')
+
+    user = int(Classes.user_input()) # Query 
+
+    if user == 0:  # Exits the application
         continue
-    elif user == 1:
+
+    elif user == 1: # Add new product 
+ 
         os.system('cls')
+
         name = '0'
+        price = ''
+        id_num = ''
+
         dup = True
 
-        while name.isdigit() or dup:
+        while name.isdigit() or dup: 
             name = input('\nEnter Product Name: ')
-            dup = new_inventory.check_duplicate(name,None)
+            dup = Classes.new_inventory.check_duplicate(None,name) # Checks if name already in Inventory
+
             if name.isdigit():
-                print('\nInvalid name, must be letters!')
+                print('\nInvalid name, must be letters!') 
 
             if dup:
                 print('\nName already taken!')
-            time.sleep(.5)
+
+            time.sleep(.25)
             os.system('cls')
             
-        id_num = ''
+        
+
         while not id_num.isdigit() or dup:
             id_num = input('\nEnter Product ID Here: ')
-            dup = new_inventory.check_duplicate(None,int(id_num))
+            dup = Classes.new_inventory.check_duplicate(int(id_num),None) # Checks if ID already in inventory
+
             if not id_num.isdigit():
                 print('\nInvalid ID, must be numbers!')
+
             if dup:
                 print('\nID already taken!')
-            time.sleep(.5)
+
+            time.sleep(.25)
             os.system('cls')
         
-        price = ''
+        
+
         while not price.isdigit():
             price = input('\nEnter Price Here: ')
+
             if not id_num.isdigit():
                 print('\nInvalid price, must be numbers!')
-            time.sleep(.5)
+
+            time.sleep(.25)
             os.system('cls')
 
-        prod = Classes.Product(name,int(id_num),int(price),0)
-        new_inventory.add(prod)
+        prod = Classes.Product(name,int(id_num),int(price),0) # Creates the object 
+        Classes.new_inventory.add(prod) # Adds the object in list/inventory
+
         print(prod)
         print('\nProduct successfully added!')
 
     elif user == 2: # Delete existing products 
-        if len(new_inventory.products) == 0:
-            print('\nNo such product found in inventory!')
-            time.sleep(.5)
+
+        if Classes.new_inventory.get_len() == 0: 
             continue
-        
+
         id_num = ''
         bool = False
+
         while not id_num.isdigit() or not bool:
             id_num = input('\nEnter Product ID Here: ')
+
             if not id_num.isdigit():
+
                 print('\nMust be numbers!')
-                time.sleep(.5)
+                time.sleep(.25)
                 os.system('cls')
                 continue
-            bool = new_inventory.delete_product(int(id_num))
-            time.sleep(.5)
+
+            bool = Classes.new_inventory.delete_product(int(id_num)) # Deletes the product
+            time.sleep(.25)
             os.system('cls')
+
+    elif user == 3: # Clears the entire inventory
+        Classes.new_inventory.products.clear()
+        print('\nAll products successfully cleared!')
                 
-    elif user == 3: # add quantity 
+    elif user == 4: # Add quantity to existing product
+
+        if Classes.new_inventory.get_len() == 0: 
+            continue
+
         os.system('cls')
         id_num = ''
         quantity = ''
         bool = False
-        while not id_num.isdigit() or not bool:
+
+        while not id_num.isdigit() or not bool: 
             id_num = input('\nEnter Product ID Here: ')
-            bool = new_inventory.check_duplicate(None,int(id_num))
+            bool = Classes.new_inventory.check_duplicate(int(id_num),None) # Checks if product is in inventory
+
             if not id_num.isdigit():
                 print('\nMust be numbers!')
+
             if not bool:
                 print('\nProduct not found!')
-            time.sleep(.5)
+
+            time.sleep(.25)
             os.system('cls')        
+
         while not quantity.isdigit():
+            product = Classes.new_inventory.product_display(int(id_num)) # Displays product info in terminal
             quantity = input('\nEnter Amount Here: ')
+
             if not quantity.isdigit():
                 print('\nMust be numbers!')
-            time.sleep(.5)
+
+            time.sleep(.25)
             os.system('cls')
-        new_inventory.add_quantity(int(quantity),int(id_num))
+
+        Classes.new_inventory.add_quantity(int(quantity),int(id_num),product) # Adds quantity 
 
         
-    elif user == 4: # Remove product quantity 
+    elif user == 5: # Deposits product quantity 
+        if Classes.new_inventory.get_len() == 0: 
+            continue
+        time.sleep(.25)
+        os.system('cls')
 
         id_num = ''
         quantity = ''
         bool = False
-        while not id_num.isdigit() or not bool:
 
+        while not id_num.isdigit() or not bool:
             id_num = input('\nEnter Product ID Here: ')
-            bool = new_inventory.check_duplicate(None,int(id_num))
+            bool = Classes.new_inventory.check_duplicate(int(id_num),None) # Checks if product in inventory 
 
             if not id_num.isdigit():
                 print('\nMust be numbers!')
                 
             if not bool:
                 print('\nProduct not found!')
-            time.sleep(.5)
+            time.sleep(.25)
             os.system('cls')
-        bool = False
-        while not quantity.isdigit() or not bool:
 
-            for _ in new_inventory.products:
-                if _.id_number == int(id_num):
-                    print('\n')
-                    print(_)   
+        bool = False
+
+        while not quantity.isdigit() or not bool:
+            product = Classes.new_inventory.product_display(int(id_num))
 
             quantity = input('\nEnter Amount Here:')
+
             if not quantity.isdigit():
+
                 print('\nMust be numbers!')
-                time.sleep(.5)
+                time.sleep(.25)
                 os.system('cls')
                 continue
-            bool = new_inventory.minus_quantity(int(quantity),int(id_num))
+
+            time.sleep(.25)
+            os.system('cls')
+            bool = Classes.new_inventory.minus_quantity(int(quantity),int(id_num),product) # Deposits quantity 
+
             if not bool:
-                break 
+                continue 
 
 
     
-    elif user == 5: # Edit product
-        os.system('cls')
-        if len(new_inventory.products) == 0: 
-            print('\nNo such product found in inventory!')
+    elif user == 6: # Edit product information
+        if Classes.new_inventory.get_len() == 0: 
             continue
+        
         id_num = ''
+        index = ''
+        os.system('cls')
+
+
         bool = False
+
         while not id_num.isdigit() or not bool:
             id_num = input('\nEnter Product ID: ')
+
             if not id_num.isdigit():
+
                 print('\nMust be numbers!')
-                time.sleep(.5)
+                time.sleep(.25)
                 os.system('cls')
                 continue
-            bool = new_inventory.check_duplicate(None,int(id_num))
+
+            bool = Classes.new_inventory.check_duplicate(int(id_num),None)
+
             if not bool:
                 print('\nProduct not found!')
-            time.sleep(.5)
+            time.sleep(.25)
             os.system('cls')
-        index = ''
+
 
         while True:
+            product = Classes.new_inventory.product_display(int(id_num)) # Displays newly edited product information in terminal
 
+            x = Classes.new_inventory.product_rename(int(id_num),product)
+            
+            if x == 0: break
 
-            index = input('\n[0] Exit\n[1] Edit name\n[2] Edit Product ID\n[3] Edit Price\nENTER: ')
-            if int(index) == 0:
-                break
-            new_inventory.product_rename(int(id_num),int(index))  
-            time.sleep(.5)
-
+            time.sleep(.25)
             os.system('cls')
-            id_num = _.id_number
+            id_num = product.id_number # Updates id_number if edited
 
-    elif user == 6:
-        os.system('cls')
-        if len(new_inventory.products) == 0:
-            print('\nNo product in inventory yet!')
+    elif user == 7: # View single product
+
+
+
+        if Classes.new_inventory.get_len() == 0: 
             continue
         id_num = ''
+
         bool = False
-        while not id_num.isdigit() or not bool: # BUG": cannot find the product
+        
+        while not id_num.isdigit() or not bool: 
+            os.system('cls')
             id_num = input('\nEnter Product ID: ')
+
             if not id_num.isdigit():
                 print('\nMust be numbers!')
                 continue
-            bool = new_inventory.check_duplicate(None,int(id_num))
+
+            bool = Classes.new_inventory.check_duplicate(int(id_num),None)
+
             if not bool:
                 print('\nProduct not found!')
+        time.sleep(.5)
         os.system('cls')
-        for _ in new_inventory.products:
-            if _.id_number == int(id_num):
-                print(_)   
-        
+        product = Classes.new_inventory.product_display(int(id_num))
 
+    elif user == 8: # View all products in inventory
 
-    
-    elif user == 7:
-        if len(new_inventory.products) == 0:
-            print('No product in inverntory!')
+        if Classes.new_inventory.get_len() == 0: 
             continue
+
         os.system('cls')
-        for _ in new_inventory.products:
+
+        for _ in Classes.new_inventory.products:
             print(_)
             print('\n')
     
